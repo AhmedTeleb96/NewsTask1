2705af75afc58029f1cbe3bd683f5527a0d54297
Ahmed Teleb <ahmedasd37@gmail.com>
2022-12-17 16:17:25 +0200

    hilt main code setup

--- app/src/main/java/com/ahmedteleb/newstask1/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ahmedteleb/newstask1/presentation/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.ahmedteleb.newstask1
+package com.ahmedteleb.newstask1.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmedteleb.newstask1.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ahmedteleb/newstask1/presentation/main/NewsApplication.kt
@@ -0,0 +1,10 @@
+package com.ahmedteleb.newstask1.presentation.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApplication : Application()
+{
+
+}
